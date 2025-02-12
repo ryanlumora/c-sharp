72f3420a813677e5536c9d8d2d62daaa1dfc516f
Ryan Lucas <ryanlucaspatron@gmail.com>
2025-02-12 20:44:33 -0300

    Feat: Creation of first lines to Storage Manager CRUD Project

--- crud-app/Program.cs
+++ crud-app/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+
+📦 Controle de Estoque
+📌 Descrição: Um sistema para cadastrar produtos e controlar quantidades em estoque.
+🛠 Tecnologias sugeridas: ASP.NET Core MVC, Entity Framework, SQL Server.
+🎯 Funcionalidades:
+
+Adicionar novos produtos (nome, descrição, preço, quantidade).
+Editar informações dos produtos.
+Listar todos os produtos do estoque.
+Excluir produtos.
+
+*/
+
+Console.WriteLine(@"
+Ｃｏｎｔｒｏｌｅ Ｄｅ Ｅｓｔｏｑｕｅ
+");
+
+Console.Write("Digite qualquer tecla para sair...");
+Console.ReadKey();
+
